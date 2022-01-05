15bec89c296a7c7ead4ac1a4becd49f33ae6a350
kellykang-tech <kellykang13@gmail.com>
2022-01-05 11:16:49 +0900

    :memo: Add: @EmbeddedId 사용

--- model4/src/main/java/com/jpa/tutorial/compositeprimarykey/Main.java
+++ model4/src/main/java/com/jpa/tutorial/compositeprimarykey/Main.java
@@ -16,23 +16,17 @@ public class Main {
             tx.begin();
             // business logic
             final Parent parent = new Parent();
-            parent.setId1("myId1");
-            parent.setId2("myId2");
-            parent.setName("parentName");
-
-            final Parent parent2 = new Parent();
-            parent2.setId1("myId2");
-            parent2.setId2("myId9");
-            parent2.setName("parentName2");
-
-            em.persist(parent);
-            em.persist(parent2);
-
             final ParentId parentId = new ParentId("myId1", "myId2");
+
+            parent.setId(parentId);
+            parent.setName("parentName");
+            em.persist(parent);
+
             final Parent findParent = em.find(Parent.class, parentId);
             System.out.println("findParent.getName() = " + findParent.getName());
 
-
+            final ParentId parentIdCopy = new ParentId("myId1", "myId2");
+            System.out.println("equals method test = " + parentId.equals(parentIdCopy));
             tx.commit();
 
         } catch (Exception e) {
--- model4/src/main/java/com/jpa/tutorial/compositeprimarykey/ParentId.java
+++ model4/src/main/java/com/jpa/tutorial/compositeprimarykey/ParentId.java
@@ -1,12 +1,17 @@
 package com.jpa.tutorial.compositeprimarykey;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Embeddable
 public class ParentId implements Serializable {
 
+    @Column(name = "PARENT_ID1")
     private String id1;
 
+    @Column(name = "PARENT_ID2")
     private String id2;
 
 
